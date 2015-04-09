279065e2fb19f85584b15010bfec2e38bfa3373e
mattycao <mattycao@gmail.com>
2015-04-08 20:25:41 -0700

    week3 Day2 supplement
    add the supplement materials

---
+++ week3_Day1/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by caoyangkaka on 4/8/15.
+ */
+//var a =false, b = 9, c = 'a';
+//var result = c && b && a;
+//console.log(result);
+
+var num = setTimeout(function(){console.log(1)}, 1000);
+console.log(num.valueOf());
