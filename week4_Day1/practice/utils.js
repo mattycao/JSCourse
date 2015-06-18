56f0af80b291953280384ad9d97dce604cb51e35
mattycao <mattycao@gmail.com>
2015-06-18 15:50:59 -0700

    week 4 about combine the animate function

--- week4_Day1/practice/utils.js
+++ week4_Day1/practice/utils.js
@@ -2,11 +2,22 @@
  * Created by caoyangkaka on 6/18/15.
  */
 utils = {};
-utils.getCss = function(ele, attr) {
+utils.getCss = function (ele, attr) {
     try {
         // notice here, the getComputedSyle is the attribute of window, please
         return parseFloat(window.getComputedStyle(ele, null)[attr]);
-    } catch(e) {
-        return parseFloat(ele.currentStyle[attr]);
+    } catch (e) {
+        if (attr === 'opacity') {
+            // for the opacity filter in ie
+            var reg = /^alpha\(opacity=(\d+)\)$/;
+            if (reg.test(ele.currentStyle.filter)) {
+
+                return RegExp.$1 / 100; // keep the unit
+            } else {
+                return 1;
+            }
+        } else {
+            return parseFloat(ele.currentStyle[attr]);
+        }
     }
 }
